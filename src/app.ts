efbee481e1a0366fbc89146ce7551428d5b7ccd2
Arjan Dhakal <dhakal.arjan@gmail.com>
2023-10-30 20:21:11 +0545

    - [x] Added POST request for movies - [x] Used sensible for http errors - [x] Refactored GET movies endpoint to use the favMovies array

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,51 @@
 import { type FastifyInstance, type FastifyPluginOptions } from 'fastify'
+import Sensible from '@fastify/sensible'
+
+interface FavMovies {
+  title: string
+  description: string
+}
+
+const favMovies: FavMovies[] = []
 
 export default async function (
   fastify: FastifyInstance,
   opts: FastifyPluginOptions,
 ): Promise<void> {
+  await fastify.register(Sensible)
+
   fastify.route({
     url: '/movies',
     method: 'GET',
     handler: function myHandler(request, reply) {
-      reply.send('movies list')
+      reply.send({
+        message: 'Movies listed successfully',
+        success: true,
+        data: favMovies,
+      })
+    },
+  })
+
+  fastify.route({
+    url: '/movies',
+    method: 'POST',
+    handler: function handler(request, reply) {
+      const data = request.body as FavMovies
+      if (!data?.title || !data?.description) {
+        throw fastify.httpErrors.badRequest(
+          'Please ensure both title and description is provided',
+        )
+      }
+      favMovies.push({
+        title: data.title,
+        description: data.description,
+      })
+
+      reply.send({
+        message: 'Movie added succesfully',
+        success: true,
+        data: null,
+      })
     },
   })
 }
